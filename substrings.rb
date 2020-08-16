12443d1f9719b045f6144c6c7dbd28d3b427cfdd
Sanjana <sanjhemdev@gmail.com>
2020-08-16 22:49:25 +0530

    Initialize file

---
+++ substrings.rb
@@ -0,0 +1,10 @@
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+print "Gimme a string to check: "
+inp_string = gets.chomp
+
+def substrings(str,dict)
+  
+end
+
+puts substrings(inp_string,dictionary)
