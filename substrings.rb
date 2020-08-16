b670f929e42a7332defc23e8620c63af1dc5063e
Sanjana <sanjhemdev@gmail.com>
2020-08-17 04:36:27 +0530

    Add ability to accept dictionary as input + Comment the functionality of all code blocks

--- substrings.rb
+++ substrings.rb
@@ -1,9 +1,14 @@
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-
+# Howdy partner, sit down! How's it going? <= Taken from the TOP site to use for testing
 print "Gimme a string to check: "
 inp_string = gets.chomp
 
-def substrings(str,dict)
+# below,down,go,going,horn,how,howdy,it,i,low,own,part,partner,sit <= Taken from the TOP site to use for testing
+print "Gimme a list of words to check against (of the type 'a,b,c, ...): "
+inp_dictionary = gets.chomp
+dictionary = inp_dictionary.split(",")
+
+# Function to find the words in the dictionary present in the inp_string and return a hash with the words and the number of times they appear; Function maintains the order of words/sub strings in the inputted dictionary
+def substrings(str, dict)
   results = Hash[dict.map { |word| [word, 0] }]
   str_arr = str.downcase.split(" ")
   str_arr.each do |i|
@@ -16,4 +21,4 @@ def substrings(str,dict)
   results.keep_if { |k,v| v > 0 }
 end
 
-puts substrings(inp_string,dictionary)
+puts substrings(inp_string, dictionary)
