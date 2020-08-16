e5797bbca4a3e76cb42b15cf6888c140d2c401f6
Sanjana <sanjhemdev@gmail.com>
2020-08-17 02:24:25 +0530

    Complete Sub Strings Project

--- substrings.rb
+++ substrings.rb
@@ -4,7 +4,16 @@ print "Gimme a string to check: "
 inp_string = gets.chomp
 
 def substrings(str,dict)
-  
+  results = Hash[dict.map { |word| [word, 0] }]
+  str_arr = str.downcase.split(" ")
+  str_arr.each do |i|
+    dict.each do |j|
+      if i.include?(j)
+        results[j] += 1
+      end
+    end
+  end
+  results.keep_if { |k,v| v > 0 }
 end
 
 puts substrings(inp_string,dictionary)
